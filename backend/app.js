cc22a02e121926854f44084efb58e1c4c210c374
Pham Anh Kiet <kapham231@gmail.com>
2026-05-20 22:33:36 +0700

    fix: update root API endpoint to handle multiple routes

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/uploads", express.static("uploads")); // To serve uploaded files in Frontend production
 
-app.get("/", (req, res) => {
+app.get(["/", "/api"], (req, res) => {
     res.send("AI Chat API is running...");
 });
 
