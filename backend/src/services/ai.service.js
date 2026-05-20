cd2efde01965ed93f8b9903b5c75ba60ab1b8b34
Pham Anh Kiet <kapham231@gmail.com>
2026-05-20 20:41:54 +0700

    fix: change model to optimize request and token

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -8,7 +8,7 @@ const client = new OpenAI({
 export const generateAIResponse = async (messages) => {
     try {
         const completion = await client.chat.completions.create({
-            model: "llama-3.3-70b-versatile",
+            model: "llama-3.1-8b-instant",
             messages: messages,
         });
 
