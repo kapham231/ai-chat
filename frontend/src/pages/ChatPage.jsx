8d9444c43629ad47240bcc73ff42f9e5cd05d9aa
Pham Anh Kiet <kapham231@gmail.com>
2026-05-20 19:50:44 +0700

    fix: dont show loading when fetch message in current conversation

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -19,7 +19,7 @@ const ChatPage = () => {
         fetchConversations();
     }, []);
 
-    const fetchConversations = async () => {
+    const fetchConversations = async (skipMessageLoad = false) => {
         try {
             const response = await api.get("/conversations");
             // console.log(response.data);
@@ -31,7 +31,9 @@ const ChatPage = () => {
                 const activeConv = convs.find(c => c._id === savedId);
                 if (activeConv) {
                     setSelectedConversation(activeConv);
-                    await fetchMessages(activeConv._id);
+                    if (!skipMessageLoad) {
+                        await fetchMessages(activeConv._id);
+                    }
                 }
             }
         } catch (error) {
@@ -133,7 +135,7 @@ const ChatPage = () => {
                 assistantMessage,
             ]);
 
-            fetchConversations();
+            fetchConversations(true);
         } catch (error) {
             console.error(error);
             setMessages((prev) => prev.filter(m => m._id !== tempUserMessage._id));
